77d0cc70cf1673694fd4d873145d0d9ce8af5951
IT21223808 <IT21223808@my.sliit.lk>
2025-11-07 23:25:49 +0530

    updated

--- src/Components/Documents.tsx
+++ src/Components/Documents.tsx
@@ -95,7 +95,7 @@ export default function Documents() {
               className="file-card bg-white shadow-md rounded-lg p-5 w-full max-w-xs flex flex-col items-center hover:shadow-xl transition"
             >
               {/* Use Next.js Image component for optimization */}
-              <Image
+              <img
                 src="https://cdn-icons-png.flaticon.com/512/337/337946.png"
                 alt="File icon"
                 width={56} // Set width and height for the image
